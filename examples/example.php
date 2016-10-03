851bd08aa39d4b24f9f8b42ffc4909d5654c3ed0
James <Incrementing@users.noreply.github.com>
2016-10-03 07:43:38 +0100

    Added user-agent
    User-agents are needed to make requests to api.mcuuid.com!

--- examples/example.php
+++ examples/example.php
@@ -18,6 +18,7 @@ $ch = curl_init('https://api.mcuuid.com/json/uuid/notch');
 curl_setopt($ch, CURLOPT_VERBOSE, true);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 $res = curl_exec($ch);
 
 if (!$res) {
